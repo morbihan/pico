a10e2434c31925fdcb5863bd76cfee1e89d0bfe3
Jeremy <j_juist@hotmail.com>
2021-05-26 21:06:38 +0200

    Added printing on off for serial text output.

--- blink.py
+++ blink.py
@@ -6,8 +6,10 @@ led_onboard = machine.Pin(25, machine.Pin.OUT)
 
 while True:
     led_onboard.value(1)
+    print ("On")
     utime.sleep(0.75)
     led_onboard.value(0)
+    print ("Off")
     utime.sleep(0.75)
 #end
     
